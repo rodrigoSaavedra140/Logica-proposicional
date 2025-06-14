099eac5acfdd577a501bd789ed838d0a7255e7fa
Saavedra Diaz Hugo Rodrigo <hugorodrigo.saavedradiaz@gmail.com>
2025-06-14 19:37:06 -0300

    change in the divideProposition funtion

--- script/buttons.js
+++ script/buttons.js
@@ -1,23 +1,43 @@
 
 const screen = document.querySelector(".screen");
 
+const regex = /(¬\(([^()]|\([^()]*\))*\)|¬[^()]|\(([^()¬]|\([^()]*\))*\)|[^()¬]+)/g;
+var parenthesis = '';
+
 function appendToScreen(input){
-    screen.value += input; 
+    //condicion para agregar los parentesis o otro signo, dependiendo si ya se abrio o cerro anteriormente
+    if(input === 's'){
+        if (parenthesis === '(' ){
+            screen.value += ')';
+            parenthesis = ')';
+        }else {
+            if(!isNaN(screen.value[screen.value.length -1])){
+                screen.value += '*';
+            }
+            screen.value += '('
+            parenthesis = '(';
+        }
+    }else{
+        screen.value += input; 
+    } 
 }
 
 function clearScreen() {
     screen.value = "";
-    hist.innerText = "";
+    
     parenthesis = '';
 }
 // limpia solo el ultimo caracter del input
 function delScreen (){
     screen.value = screen.value.slice(0,-1);
 }
-const regex = /(¬\(([^()]|\([^()]*\))*\)|¬[^()]|\(([^()¬]|\([^()]*\))*\)|[^()¬]+)/g;
+
 function equal (){
-    a = "a(¬(¬(¬(poq)))0¬ay(pyq))";
-    b = a.match(regex);
+    divideProposition(screen.value);
+}
+
+function divideProposition (input){
+    const a = input.match(regex);
     console.log(a);
-    console.log(b);
+    // hacer una funcion que separe la proposicion en proposiciones reducidas en un array
 }
--- script/logic.js
+++
@@ -1,9 +0,0 @@
-
-const regex = /(¬\(([^()]|\([^()]*\))*\)|¬[^()]|\(([^()¬]|\([^()]*\))*\)|[^()¬]+)/g;
-
-export function divideProposition (a){
-    a = "%(pyq)o(poq)";
-    a.split(regex);
-    console.log(a)
-    // hacer una funcion que separe la proposicion en proposiciones reducidas en un array
-}
